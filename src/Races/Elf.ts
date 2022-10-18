3bf0d9b6ea995a20611f47e18005996770928d89
Ckauark <ckauark@gmail.com>
2022-10-18 16:59:54 -0300

    feat: create subclasses from Race

---
+++ src/Races/Elf.ts
@@ -0,0 +1,19 @@
+import Race from './Race';
+
+export default class Elf extends Race {
+  private _maxLifePoints = 99;
+  private static raceInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    Elf.raceInstances += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf.raceInstances;
+  }
+}
