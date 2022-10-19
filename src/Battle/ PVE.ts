50b90a17bcdb5223486885a986a12ceddf007254
Ckauark <ckauark@gmail.com>
2022-10-19 00:09:22 -0300

    feat: pve class

--- src/Battle/ PVE.ts
+++ src/Battle/ PVE.ts
@@ -1,17 +1,25 @@
 import Character from '../Character';
-import Monster from '../Monster';
+import Fighter, { SimpleFighter } from '../Fighter';
+// import Monster from '../Monster';
 import Battle from './Battle';
 
 export default class PVE extends Battle {
-  constructor(player1: Character, protected monster: Monster[]) {
+  constructor(
+    player1: Fighter,
+    protected monsters: SimpleFighter[],
+  ) {
     super(player1);
   }
 
   fight(): number {
-    while (this.player2.lifePoints !== -1 || this.player.lifePoints !== -1) {
-      this.player2.attack(this.player);
-      if (this.player.lifePoints === -1) break;
-      this.player.attack(this.player2);
+    while (this.player.lifePoints !== -1) {
+      this.monsters.forEach((monster) => {
+        monster.attack(this.player);
+      }); 
+      if (this.monsters.every(({ lifePoints }) => lifePoints === -1)) break;
+      this.monsters.forEach((monster) => {
+        this.player.attack(monster);
+      });
     }
     return super.fight();
   }
