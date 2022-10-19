b4faac3c63b715be6eb12b9a1b8d7dd7ef778716
Ckauark <ckauark@gmail.com>
2022-10-19 00:17:36 -0300

    refactor: PVP class

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,13 +1,13 @@
-import Character from '../Character';
+import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  constructor(player1: Character, protected player2: Character) {
+  constructor(player1: Fighter, protected player2: Fighter) {
     super(player1);
   }
 
   fight(): number {
-    while (this.player2.lifePoints !== -1 || this.player.lifePoints !== -1) {
+    while (this.player2.lifePoints !== -1) {
       this.player2.attack(this.player);
       if (this.player.lifePoints === -1) break;
       this.player.attack(this.player2);
